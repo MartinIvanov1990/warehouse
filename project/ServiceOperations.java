fd22072ec44be68a209acd8da30601190102ce81
bestappsdevelopersteam <bestappsdevelopersteam@gmail.com>
2022-12-04 09:13:33 +0200

    Some spell correction

--- project/ServiceOperations.java
+++ project/ServiceOperations.java
@@ -1,7 +1,7 @@
 package warehouse.project;
 
 public enum ServiceOperations {
-    PRINT_ALL_PRODUCTS(1), PRINT_PRODUCT_BY_OPTION(2)
+    PRINT_ALL_PRODUCTS (1), PRINT_PRODUCT_BY_OPTION (2)
 
     private int operationNumber;
 
